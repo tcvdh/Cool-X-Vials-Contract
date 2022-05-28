f30ee531011644b983b5c6653c9d95085910e6bf
tcvdh <tcvdh@outlook.com>
2022-05-28 22:04:16 +0200

    whitelist removal change

--- CoolXVials.sol
+++ CoolXVials.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+/*
+ ___  ___  ________  ________  ___       ___  ___  ________      
+|\  \|\  \|\   __  \|\   ___ \|\  \     |\  \|\  \|\   __  \     
+\ \  \\\  \ \  \|\  \ \  \_|\ \ \  \    \ \  \\\  \ \  \|\  \    
+ \ \   __  \ \  \\\  \ \  \ \\ \ \  \    \ \   __  \ \  \\\  \   
+  \ \  \ \  \ \  \\\  \ \  \_\\ \ \  \____\ \  \ \  \ \  \\\  \  
+   \ \__\ \__\ \_______\ \_______\ \_______\ \__\ \__\ \_____  \ 
+    \|__|\|__|\|_______|\|_______|\|_______|\|__|\|__|\|___| \__\
+                                                            \|__|
+*/
 
+pragma solidity ^0.8.0;
 
 import "./ERC1155.sol";
 import "./Ownable.sol";
@@ -17,11 +27,8 @@ contract CoolXVials is ERC1155, Ownable, PaymentSplitter {
   uint256 public totalSupply = 0;
   uint256 public price = 0.1 ether;
   bool public paused = true;
-  CoolCats public CC = CoolCats(0x34570e0Cb7EA8e1609B3B703D7Df7026d83a1Fdf);
+  CoolCats public CC = CoolCats(0x1A92f7381B9F03921564a437210bB9396471050C);
   CloneX public CX = CloneX(0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B);
-  uint256 public whitelistTimestamp;
-  uint256 public totalPublicWhitelistMints;
-  uint256 public totalWhitelistMints;
 
   constructor(
     string memory _URI, 
@@ -35,43 +42,28 @@ contract CoolXVials is ERC1155, Ownable, PaymentSplitter {
 
   function mint(address _to, uint256 _amount) public payable {
     require(totalSupply + _amount <= maxSupply && _amount > 0);
-    require(balanceOf(_to, 1) + _amount <= 5);
-
-    if(block.timestamp < whitelistTimestamp) {
-        require((totalPublicWhitelistMints + _amount) < (maxSupply / 2));
-        totalPublicWhitelistMints += _amount;
-    }
 
     if(msg.sender != owner()) {
-        require(!paused);
-        require(msg.sender == _to);
-        require(msg.value >= price * _amount);
+      if(CC.walletOfOwner(_to).length > 0 || CX.tokensOfOwner(_to).length > 0) {
+        require(balanceOf(_to, 1) + _amount <= 10);
+      } else {
+        require(balanceOf(_to, 1) + _amount <= 5);
+      }
+      require(!paused);
+      require(msg.sender == _to);
+      require(msg.value >= price * _amount);
     }
 
     _mint(_to, 1, _amount, "");
     totalSupply += _amount;
   }
 
-  function mintWhitelist(address _to, uint256 _amount) public payable {
-    require(!paused);
-    require(CC.walletOfOwner(_to).length > 0 || CX.tokensOfOwner(_to).length > 0);
-    require(block.timestamp < whitelistTimestamp);
-
-    require((totalWhitelistMints + _amount) < (maxSupply / 2));
-    require(totalSupply + _amount <= maxSupply && _amount > 0);
-    require(balanceOf(_to, 1) + _amount <= 5);
-
-    require(msg.sender == _to);
-    require(msg.value >= price * _amount);
-    
-    _mint(_to, 1, _amount, "");
-    totalWhitelistMints += _amount;
-    totalSupply += _amount;
-  }
-
-
-  function burn(uint _amount) public {
-    _burn(msg.sender, 1, _amount);
+  function burn(address account, uint256 _amount) public virtual {
+    require(
+      account == _msgSender() || isApprovedForAll(account, _msgSender()),
+      "ERC1155: caller is not owner nor approved"
+    );
+    _burn(account, 1, _amount);
     totalSupply -= _amount;
   }
 
@@ -83,8 +75,12 @@ contract CoolXVials is ERC1155, Ownable, PaymentSplitter {
     paused = _state;
   }
 
-  function setWhitelistTimestamp(uint256 _timestamp) public onlyOwner {
-    whitelistTimestamp = _timestamp;
+  function setPrice(uint256 _price) public onlyOwner {
+    price = _price;
+  }
+
+  function setMaxSupply(uint256 _maxSupply) public onlyOwner {
+    maxSupply = _maxSupply;
   }
 }
     
